64243e3ba6a0d53bfca440e204ab10372a1fc83e
Marinkie <69327534+Marinkiet@users.noreply.github.com>
2022-08-20 18:43:42 +0200

    heroku 3

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
+app.use(express.static(__dirname + '/dist/potfolio'));
+app.get('/*', function(req,res)
 
-
-app.use(express.static(__dirname + '/dist/heroku-deploy'));
-
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/heroku-deploy/index.html'));
-});
+{res.sendFile(path.join(__dirname+'/dist/potfolio/index.html'));});
 
 app.listen(process.env.PORT || 8080);
