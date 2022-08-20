eab193dbfcced204cc1b0727f33f7868af5451e7
Marinkie <69327534+Marinkiet@users.noreply.github.com>
2022-08-20 18:28:44 +0200

    update 2 heroku

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+
+app.use(express.static(__dirname + '/dist/heroku-deploy'));
+
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/dist/heroku-deploy/index.html'));
 });
 
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 8080);
